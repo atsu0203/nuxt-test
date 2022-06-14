f1d1596f8abf35e65b9d2ec8e9cccd37efff1145
a_kiritani <a.kiritani@skillengine.jp>
2022-06-14 23:59:52 +0900

    画面追加

---
+++ pages/products/listProduct.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+  <table>
+    <tr>
+      <th>商品番号</th>
+      <th>商品名</th>
+      <th>詳細</th>
+      <th>編集</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>テスト1</td>
+      <td><NuxtLink to=/products/1/showProduct>詳細</NuxtLink></td>
+      <td><NuxtLink to=/products/1/editProduct>編集</NuxtLink></td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>テスト2</td>
+      <td><NuxtLink to=products/2/showProduct>詳細</NuxtLink></td>
+      <td><NuxtLink to=products/2/editProduct>編集</NuxtLink></td>
+    </tr>
+    <tr>
+    <td>3</td>
+      <td>テスト3</td>
+      <td><NuxtLink to=products/3/showProduct>詳細</NuxtLink></td>
+      <td><NuxtLink to=products/3/editProduct>編集</NuxtLink></td>
+    </tr>
+ </table>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
