909f92002311130f499dd1b9a8d3c5ff486205ec
a_kiritani <a.kiritani@skillengine.jp>
2022-06-17 23:48:22 +0900

    商品詳細の変更

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,7 @@
   <td>テスト</td>
   <td><NuxtLink to=/products/1/showProduct>詳細</NuxtLink></td>
   <td><NuxtLink to=/products/1/editProduct>編集</NuxtLink></td>
+  <td><NuxtLink to=/products/1/editProduct>編集</NuxtLink></td>
   </tr>
   </table>
 
