46dc7173be07e9a1535cbddd3e18b14dd77120bc
a_kiritani <a.kiritani@skillengine.jp>
2022-06-19 22:38:15 +0900

    一覧画面の編集

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,8 @@
   <td><NuxtLink to=/products/1/showProduct>詳細</NuxtLink></td>
   <td><NuxtLink to=/products/1/editProduct>編集</NuxtLink></td>
   <td><NuxtLink to=/products/1/editProduct>編集</NuxtLink></td>
+   <td><NuxtLink to=/products/1/editProduct>編集</NuxtLink></td>
+
   </tr>
   </table>
 
